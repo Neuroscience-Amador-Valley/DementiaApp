e916f4e0859cf9970ddc7c42dc7869d1d48e575c
Ellie Sara Huang <ellie_sara@outlook.com>
2020-11-17 16:26:37 -0800

    fixed the HomePage and the Physical view to look presentable

--- Neuroscience-DementiaApp/HomePage.swift
+++ Neuroscience-DementiaApp/HomePage.swift
@@ -11,43 +11,108 @@ import SwiftUI
 struct HomePage: View {
     
     @State private var physical = false
+    @State private var mental = false
+    @State private var social = false
     
     var body: some View {
-        //        NavigationView {
-        //            NavigationLink(destination: Physical()) {
-        //                HStack {
-        //                    Image(systemName: "thermometer").frame(width: 50, height: 100)
-        //                    Text("Neuroscience").font(.headline)
-        //                }.navigationBarTitle(Text("October 6, 2020"))
-        //            }
-        //        }
         GeometryReader { geometry in
+            
             NavigationView {
+                
                 VStack {
+                    
                     HStack {
+                        
                         Spacer()
-                        Button(action: { self.physical = true }) {
-                            HStack {
-                                Image(systemName: "thermometer")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: geometry.size.width*0.1, height: geometry.size.height*0.05)
-                                    .padding(.top)
-                                    .padding(.bottom)
-                                Text("Physical")
-                                    .font(.system(size: geometry.size.width*0.07))
-                                    .fontWeight(.bold)
-                                    .padding(.trailing)
+                        
+                        VStack {
+                            
+                            Button(action: { self.physical = true }) {
+                                
+                                HStack {
+                                    Text("Physical")
+                                        .font(.system(size: geometry.size.width*0.07))
+                                        .fontWeight(.bold)
+                                        .frame(width: geometry.size.width*0.3)
+                                        .padding()
+                                    Image(systemName: "hand.raised")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: geometry.size.width*0.2, height: geometry.size.height*0.105)
+                                        .padding(.leading)
+                                    
+                                    Spacer()
+                                }
+                                .padding()
+                                .foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1)))
+                                .frame(width: geometry.size.width*0.9, height: geometry.size.height*0.23)
+                                .background(Color(#colorLiteral(red: 0.7568627451, green: 0.8426002264, blue: 0.8870300651, alpha: 1)))
+                                .cornerRadius(25)
+                                
                             }
-                            .padding()
-                            .foregroundColor(.black)
-                            .background(Color(.lightGray))
-                            .cornerRadius(15.0)
-                        }.sheet(isPresented: self.$physical) {
-                            Physical()
-                        }
+                            .padding(.top)
+                            .sheet(isPresented: self.$physical) {
+                                Physical()
+                            }
+                            
+                            Button(action: { self.mental = true }) {
+                                
+                                HStack {
+                                    Text("Mental")
+                                        .font(.system(size: geometry.size.width*0.07))
+                                        .fontWeight(.bold)
+                                        .frame(width: geometry.size.width*0.3)
+                                        .padding()
+                                    
+                                    Image(systemName: "eye")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: geometry.size.width*0.2, height: geometry.size.height*0.105)
+                                        .padding(.leading)
+                                    
+                                    Spacer()
+                                }
+                                .padding()
+                                .foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1)))
+                                .frame(width: geometry.size.width*0.9, height: geometry.size.height*0.23)
+                                .background(Color(#colorLiteral(red: 0.7541411519, green: 0.8313546777, blue: 0.9701576829, alpha: 1)))
+                                .cornerRadius(25)
+                                
+                            }
+                            
+                            Button(action: { self.social = true }) {
+                                
+                                HStack {
+                                    Text("Social")
+                                        .font(.system(size: geometry.size.width*0.07))
+                                        .fontWeight(.bold)
+                                        .frame(width: geometry.size.width*0.3)
+                                        .padding()
+                                    
+                                    Image(systemName: "person.3.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: geometry.size.width*0.2, height: geometry.size.height*0.105)
+                                        .padding(.leading)
+                                    
+                                    Spacer()
+                                }
+                                .padding()
+                                .foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1)))
+                                .frame(width: geometry.size.width*0.9, height: geometry.size.height*0.23)
+                                .background(Color(#colorLiteral(red: 0.9529411765, green: 0.8804875016, blue: 0.7963053584, alpha: 1)))
+                                .cornerRadius(25)
+                                
+                            }
+                            
+                            Spacer()
+                            
+                        }.navigationBarTitle(Text("October 6, 2020"))
+                        
                         Spacer()
-                    }.navigationBarTitle(Text("October 6, 2020"))
+                        
+                    }
+                    
                     Spacer()
                 }
             }
--- Neuroscience-DementiaApp/Physical.swift
+++ Neuroscience-DementiaApp/Physical.swift
@@ -11,48 +11,82 @@ import SwiftUI
 struct Physical: View {
     
     @Environment(\.presentationMode) var presentationMode
-    private let pSymptoms = ["Inability to wash body", "Trouble putting on clothes"]
-    @State var pChecks = [false, false]
+    private let pSymptoms = ["Having an unkempt appearance", "Inability to wash body", "Trouble putting on clothes", "Difficulty brushing teeth", "Inability to shave/comb hair", "Convulsions"] /// added 4 more and reordered
+    @State var pChecks = [false, false, false, false, false, false] /// added 4 more
     @State var newSymptom = false
     
-    init() {
-        UITableView.appearance().backgroundColor = #colorLiteral(red: 0.6737673472, green: 1, blue: 0.9499762451, alpha: 1)
-        UITableViewCell.appearance().backgroundColor = #colorLiteral(red: 0.6737673472, green: 1, blue: 0.9499762451, alpha: 1)
+    init() { /// added
+        UITableView.appearance().backgroundColor = #colorLiteral(red: 0.7568627451, green: 0.8426002264, blue: 0.8870300651, alpha: 1)
+        UITableViewCell.appearance().backgroundColor = #colorLiteral(red: 0.7568627451, green: 0.8426002264, blue: 0.8870300651, alpha: 1)
         UITableView.appearance().tableFooterView = UIView()
     }
     
     var body: some View {
-        ZStack {
-            VStack {
-                Text("Physical Symptoms").font(.largeTitle)
-                Text("Select all that apply")
-                List {
-                    ForEach(0..<self.pChecks.count) { i in
-                        HStack {
-                            Text(self.pSymptoms[i])
-                            Button (action: { self.pChecks[i].toggle() }) {
-                                if self.pChecks[i] {
-                                    Image(systemName: "checkmark.square.fill")
-                                }
-                                else {
-                                    Image(systemName: "square.fill")
-                                }
+        GeometryReader { geometry in /// new
+            
+            NavigationView { /// new
+                
+                ZStack {
+                    Color(#colorLiteral(red: 0.7568627451, green: 0.8426002264, blue: 0.8870300651, alpha: 1)).edgesIgnoringSafeArea(.all) /// new
+                    
+                    VStack {
+                        
+                        Text("Physical Symptoms").font(.largeTitle).fontWeight(.bold) /// added .bold
+                        Text("Select all that apply").font(.caption).foregroundColor(Color.blue)
+                            .padding(.top, geometry.size.height*0.013) /// added modifiers
+                        
+                        List {
+                            
+                            ForEach(0..<self.pChecks.count) { i in
+                                HStack {
+                                    
+                                    Text(self.pSymptoms[i])
+                                    
+                                    Spacer()  /// new
+                                    
+                                    Button (action: { self.pChecks[i].toggle() }) {
+                                        if self.pChecks[i] {
+                                            Image(systemName: "checkmark.square.fill")
+                                                .foregroundColor(Color.blue) /// new
+                                        }
+                                        else {
+                                            Image(systemName: "square.fill")
+                                                .foregroundColor(Color(#colorLiteral(red: 0.9339778938, green: 0.9339778938, blue: 0.9339778938, alpha: 1))) /// new
+                                        }
+                                    }
+                                    
+                                }.listRowBackground(Color(#colorLiteral(red: 0.7568627451, green: 0.8426002264, blue: 0.8870300651, alpha: 1))) /// new
                             }
                         }
-                    }
+                        
+                        Button (action: { self.newSymptom = true }) {
+                            
+                            HStack (alignment: .center) {
+                                
+                                Text("New Symptom")
+                                    .font(.system(size: geometry.size.width * 0.048)) /// new
+                                    .fontWeight(.bold) /// new
+                                Image(systemName: "plus")
+                                    .frame(width: geometry.size.width * 0.05,
+                                           height: geometry.size.height * 0.05) /// new
+                            }
+                            .padding() /// new
+                            .foregroundColor(Color(.white)) /// new
+                            .frame(width: geometry.size.width * 0.5,
+                                   height: geometry.size.height * 0.063) /// new
+                                .background(Color.blue) /// new
+                                .cornerRadius(10) /// new
+                            
+                        }.sheet(isPresented: self.$newSymptom) {
+                            NewSymptom()
+                        }
+                        
+                    }.navigationBarItems(
+                        leading: Button("Home") { self.presentationMode.wrappedValue.dismiss()},
+                        trailing: Button("Submit") { self.presentationMode.wrappedValue.dismiss() }
+                    )
                 }
-                Button (action: { self.newSymptom = true }) {
-                    HStack (alignment: .center) {
-                        Text("New Symptom")
-                        Image(systemName: "plus")
-                    }
-                }.sheet(isPresented: self.$newSymptom) {
-                    NewSymptom()
-                }
-            }.navigationBarItems(
-                leading: Button("Home") { self.presentationMode.wrappedValue.dismiss()},
-                trailing: Button("Submit") { self.presentationMode.wrappedValue.dismiss() }
-            )
+            }
         }
     }
 }
