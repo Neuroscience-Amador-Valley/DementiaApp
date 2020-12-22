bf5875850c0d20fd4e35bdc0e7814c099472d314
Ellie Sara Huang <ellie_sara@outlook.com>
2020-12-21 20:12:28 -0800

    Updated NewSymptoms view to show our latest progress in our app development session.

--- Neuroscience-DementiaApp/NewSymptom.swift
+++ Neuroscience-DementiaApp/NewSymptom.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct NewSymptom: View {
+    
+    @State var newSymptom: String = ""
+    @State var addSymptom: Bool = false
+    
+    @State private var newSymptomArr: [String] = []
+    
     var body: some View {
-        Text("New Symptom")
+        HStack {
+            Text("New Symptom")
+                .font(.title).fontWeight(.bold).padding(.leading, UIScreen.main.bounds.width*0.03)
+        }
+        HStack {
+            TextField("Enter new symptom name", text: $newSymptom)
+                .textFieldStyle(CustomTextFieldStyle())
+            Button(action: {
+                
+                self.newSymptomArr.append(self.newSymptom)
+                self.newSymptom = ""
+                
+            }) {
+                HStack(alignment: .center) {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: UIScreen.main.bounds.width*0.1, height: UIScreen.main.bounds.height*0.02)
+                }
+                .foregroundColor(Color.white)
+                .frame(width: UIScreen.main.bounds.width*0.15, height: UIScreen.main.bounds.height*0.037)
+                .background(Color(#colorLiteral(red: 0, green: 0.5492870212, blue: 1, alpha: 1)))
+            }
+        }
+        
+        List {
+            ForEach(0..<self.newSymptomArr.count, id:\.self) {
+                Text(self.newSymptomArr[$0])
+            }
+        }
+    }
+}
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<_Label>) -> some View {
+        configuration
+            .padding(.leading, UIScreen.main.bounds.width*0.03)
+            .padding(.top, UIScreen.main.bounds.width*0.015)
+            .padding(.bottom, UIScreen.main.bounds.width*0.015)
+            .background(Color(#colorLiteral(red: 0.9339778938, green: 0.9339778938, blue: 0.9339778938, alpha: 1)))
+            .cornerRadius(8)
     }
 }
 
